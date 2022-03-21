a7d97407e3f34cf6e659c7dfe7f10d2ad4bb6693
Dominik Slehofer <slehoferdominik@gmail.com>
2022-03-21 14:12:16 +0100

    Napůl rozbité

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from math import pi
 import tkinter as tk
 from tkinter import messagebox
 from tkinter.messagebox import showerror
+from typing_extensions import IntVar
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -13,26 +14,43 @@ class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
     name = "Absolutní Luxus"
     
+    
 
 
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
         self.bind("<Escape>", self.quit)
-        self.geometry("200x200")
+        self.geometry("400x400")
+        self.v = IntVar()
 
         self.btn = tk.Button(self, text="luxus", command=self.cosinus)
-        self.btn.grid(row=1, column=1)
+        self.btn.grid(row=1, column=2)
 
         self.freq = tk.Entry(self, validate="key", validatecommand=(self.register(self.validate), "%P"))
-        self.freq.grid(row=2, column=1)
+        self.freq.grid(row=2, column=2)
+
+        self.cas_start = tk.Entry(self,validate="key", validatecommand=(self.register(self.validate), "%P"))
+        self.cas_start.grid(row=3, column=2)
+        self.cas_konec = tk.Entry(self,validate="key", validatecommand=(self.register(self.validate), "%P"))
+        self.cas_konec.grid(row=4, column=2)
+        self.cas_zacatek = tk.Label(self, text="OD")
+        self.cas_zacatek.grid(row=3, column=1)
+        self.cas_final = tk.Label(self, text="DO")
+        self.cas_final.grid(row=4, column=1)
 
         self.hodnoty_x = []
         self.hodnoty_y = []
 
 
     def validate(self, value):
-        if len(value) == 0 or value.isnumeric():
+        if len(value) == 0 or value.isnumeric() or self.nula():
+            return True
+        else:
+            return False
+
+    def nula(self, value):
+        if value == "-":
             return True
         else:
             return False
@@ -55,9 +73,10 @@ class Application(tk.Tk):
             cisla=radek.split()
             self.hodnoty_x.append( float(cisla[0]))
             self.hodnoty_y.append(float(cisla[1]))
-            #t = np.linspace(self.hodnoty_x[0],self.hodnoty_x[-1], len(self.hodnoty_x))
-            #u = np.cos(2*pi*50*t)
-        plt.plot(self.hodnoty_x,self.hodnoty_y)
+        t = np.linspace(self.hodnoty_x[0],self.hodnoty_x[-1], len(self.hodnoty_x))
+        u = np.cos(2*pi*50*t)
+        plt.xlim(-10,10)
+        plt.plot(t,u)
         plt.grid()
         plt.show()
 
